2b8fb253c1ffcc3ab72c1afe07457d19b05f3077
Sam Cook <github@sam.net.nz>
2015-12-02 17:18:49 +0000

    Bump version

--- IdentityServer3.Shaolinq/Properties/AssemblyInfo.cs
+++ IdentityServer3.Shaolinq/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha10")]
+[assembly: AssemblyInformationalVersion("1.0.1-alpha1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
